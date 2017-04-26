db717225f2f60ba96d6208df59f8639ec2818f03
Ivan HA <hangolam@gmail.com>
2017-04-26 15:13:09 +0800

    make navbar sticky at the top

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -11,7 +11,7 @@ const NavigationBar = (props) => {
     });
 
     return (
-        <nav className='navbar navbar-default'>
+        <nav className='navbar navbar-default navbar-fixed-top'>
             <div className='container-fluid'>
                 <div className="navbar-header">
                     <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
