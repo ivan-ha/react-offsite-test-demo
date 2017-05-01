b5886fdc7382cf91af9dbbee8d75be5de19a1f89
Ivan HA <hangolam@gmail.com>
2017-05-01 20:01:41 +0800

    place sign-out button to the right

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -6,7 +6,7 @@ const Greeting = ({user, onSignOutClick}) => {
             <div className='alert alert-success' role='alert'>
                 <b>Hello! {user}</b>
             </div>
-            <button className='btn btn-danger' type='button' onClick={() => onSignOutClick()}>Sign out</button>
+            <button className='btn btn-danger pull-right' type='button' onClick={() => onSignOutClick()}>Sign out</button>
         </div>
     );
 }
