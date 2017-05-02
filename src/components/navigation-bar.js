4deb18b8467dd9f9ffb1ded18622b36e5217ca46
Ivan HA <hangolam@gmail.com>
2017-05-02 16:14:17 +0800

    move categories state into navbar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import NavigationBarItem from './navigation-bar-item'
 
-const NavigationBar = (props) => {
-    const categoryItems = props.categories.map((category) => {
+const categoriesUrl = 'http://localhost:3000/categories';
+
+class NavigationBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            categories: []
+        };
+
+        this.getCategories();
+    }
+
+    render() {
+        const categoryItems = this.state.categories.map((category) => {
+            return (
+                <NavigationBarItem
+                    key={category.id}
+                    category={category} />
+            );
+        });
+
         return (
-            <NavigationBarItem
-                key={category.text}
-                category={category} />
+            <nav className='navbar navbar-default navbar-fixed-top'>
+                <div className='container-fluid'>
+                    <div className="navbar-header">
+                        <button type="button" className="pull-left navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">HK01</a>
+                    </div>
+
+                    <div className='collapse navbar-collapse' id="bs-example-navbar-collapse-1">
+                        <ul className='nav navbar-nav'>
+                            {categoryItems}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
-    });
+    }
 
-    return (
-        <nav className='navbar navbar-default navbar-fixed-top'>
-            <div className='container-fluid'>
-                <div className="navbar-header">
-                    <button type="button" className="pull-left navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">HK01</a>
-                </div>
-
-                <div className='collapse navbar-collapse' id="bs-example-navbar-collapse-1">
-                    <ul className='nav navbar-nav'>
-                        {categoryItems}
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
-};
+    getCategories() {
+        fetch(categoriesUrl).then((response) => {
+            return response.json();
+        }).then((json) => {
+            this.setState({
+                categories: json
+            });
+        });
+    }
+}
 
 export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 import ListView from './components/list-view'
 import NavigationBar from './components/navigation-bar'
 import UserPanel from './components/user-panel';
 
-const categoriesUrl = 'http://localhost:3000/categories';
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            categories: []
-        };
-
-        this.getCategories();
-    }
-
-    getCategories() {
-        fetch(categoriesUrl).then((response) => {
-            return response.json();
-        }).then((json) => {
-            this.setState({
-                categories: json
-            });
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <NavigationBar categories={this.state.categories} />
-                <UserPanel />
-                <ListView />
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div>
+            <NavigationBar />
+            <UserPanel />
+            <ListView />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
