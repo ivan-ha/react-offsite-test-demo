86af15e82832decd8c0d9c9151677e6fd7b46abd
Ivan HA <hangolam@gmail.com>
2017-05-03 17:11:04 +0800

    separate articles reducer

---
+++ src/reducers/articles.js
@@ -0,0 +1,28 @@
+import {REQUEST_ARTICLES, RECEIVE_ARTICLES} from '../actions';
+
+const initialArticlesState = {
+    isFetching: false,
+    page: 1,
+    hasMore: true,
+    lists: []
+};
+
+const articles = (state = initialArticlesState, action) => {
+    switch (action.type) {
+        case REQUEST_ARTICLES:
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        case RECEIVE_ARTICLES:
+            return Object.assign({}, state, {
+                isFetching: false,
+                page: parseInt(action.page) + 1,
+                hasMore: action.hasMore,
+                lists: [...state.lists, ...action.lists]
+            });
+        default:
+            return state;
+    }
+};
+
+export default articles;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,5 @@
 import {combineReducers} from 'redux';
-import {REQUEST_ARTICLES, RECEIVE_ARTICLES} from '../actions';
-
-const initialArticlesState = {
-    isFetching: false,
-    page: 1,
-    hasMore: true,
-    lists: []
-};
-
-const articles = (state = initialArticlesState, action) => {
-    switch (action.type) {
-        case REQUEST_ARTICLES:
-            return Object.assign({}, state, {
-                isFetching: true
-            });
-        case RECEIVE_ARTICLES:
-            return Object.assign({}, state, {
-                isFetching: false,
-                page: parseInt(action.page) + 1,
-                hasMore: action.hasMore,
-                lists: [...state.lists, ...action.lists]
-            });
-        default:
-            return state;
-    }
-};
+import articles from './articles';
 
 const rootReducer = combineReducers({
     articles
